d57b8bc308ccb470f5968940d9c1d1d591fc6748
hansyuan <hanzzeh@gmail.com>
2016-09-16 04:04:42 -0700

    trying git commands

--- fun.sh
+++
@@ -1,2 +0,0 @@
-python fun.py > fun.txt
-subl fun.txt
--- script.sh
+++
@@ -1,8 +0,0 @@
-#explorer .
-
-#python sumOfPrimes.py > sumPrimes.txt
-python p51.py > family2.txt
-subl family2.txt
-#javac *
-#java Fib > output2.txt
-#subl output2.txt
